dd0558ee92d7ec364157957bdfc0aa5cad96fcfe
shalomyehudaf <authorrox@gmail.com>
2026-08-17 22:38:23 -0400

    Initial Android Project

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    //id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" apply false
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CalculatorApp"
+include(":app")
